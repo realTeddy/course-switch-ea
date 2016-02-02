98d988a396bc723a5ad883759628bb18dd0ae8d7
Tewodros Ayele Assefa <contact@tewodros.me>
2016-02-02 05:36:27 -0600

    no message

--- src/main/java/edu/mum/courseswitch/service/PendingSwitchService.java
+++ src/main/java/edu/mum/courseswitch/service/PendingSwitchService.java
@@ -7,14 +7,9 @@ package edu.mum.courseswitch.service;
 
 import edu.mum.courseswitch.dao.PendingSwitchDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
 public class PendingSwitchService {
     @Autowired
     private PendingSwitchDao pendingSwitchDao;
-    
-    @RequestMapping("/")
     
 }
