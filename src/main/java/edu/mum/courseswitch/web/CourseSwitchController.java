5623ee98796bd3ce6ba87a0724b51075489a3f6e
Tewodros Ayele Assefa <contact@tewodros.me>
2016-02-02 15:39:49 -0600

    no message

--- src/main/java/edu/mum/courseswitch/web/CourseSwitchController.java
+++ src/main/java/edu/mum/courseswitch/web/CourseSwitchController.java
@@ -5,24 +5,20 @@
  */
 package edu.mum.courseswitch.web;
 
-import edu.mum.courseswitch.domain.User;
 import edu.mum.courseswitch.service.RegistrationService;
-import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class CourseSwitchController {
     
     @Autowired
     private RegistrationService registrationService;
     
-    @RequestMapping(path = "/test", method = RequestMethod.GET)
-    public @ResponseBody User index(Principal principal) {
-        //registrationService.getRegistrations(principal.getName());
-        return new User("John", "Doe", "email@example.com", "passw0rd", false);
+    @RequestMapping(path = "/courseSwitch", method = RequestMethod.GET)
+    public String courseSwitch() {
+        return "courseSwitch";
     }
 }
